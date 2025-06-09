c150c548f4499e7bab223dbe5611b609723aea23
IGNP Andre Pratama Milano <andremilano16@gmail.com>
2025-06-09 09:53:28 +0700

    menambahkan mainscreen

---
+++ app/src/main/java/com/andre0016/mobpro1/ui/screen/MainScreen.kt
@@ -0,0 +1,495 @@
+package com.andre0016.mobpro1.ui.screen
+
+import android.content.ContentResolver
+import android.content.Context
+import android.content.res.Configuration
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.ImageDecoder
+import android.os.Build
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.credentials.ClearCredentialStateRequest
+import androidx.credentials.CredentialManager
+import androidx.credentials.CustomCredential
+import androidx.credentials.GetCredentialRequest
+import androidx.credentials.GetCredentialResponse
+import androidx.credentials.exceptions.ClearCredentialException
+import androidx.credentials.exceptions.GetCredentialException
+import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.andre0016.mobpro1.BuildConfig
+import com.andre0016.mobpro1.R
+import com.andre0016.mobpro1.model.GalleryItem
+import com.andre0016.mobpro1.model.User
+import com.andre0016.mobpro1.network.ApiStatus
+import com.andre0016.mobpro1.network.GalleryApi
+import com.andre0016.mobpro1.network.UserDataStore
+import com.andre0016.mobpro1.ui.theme.Mobpro1Theme
+import com.canhub.cropper.CropImageContract
+import com.canhub.cropper.CropImageContractOptions
+import com.canhub.cropper.CropImageOptions
+import com.canhub.cropper.CropImageView
+import com.google.android.libraries.identity.googleid.GetGoogleIdOption
+import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential
+import com.google.android.libraries.identity.googleid.GoogleIdTokenParsingException
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen() {
+    val context = LocalContext.current
+    val dataStore = UserDataStore(context)
+    val user by dataStore.userFlow.collectAsState(User("", "", ""))
+    val viewModel: MainViewModel = viewModel()
+    val errorMessage by viewModel.errorMessage
+
+    var showDialog by remember { mutableStateOf(false) }
+    var showItemDialog by remember { mutableStateOf(false) }
+
+    var bitmap: Bitmap? by remember { mutableStateOf(null) }
+    val launcher = rememberLauncherForActivityResult(CropImageContract()) {
+        bitmap = getCroppedImage(context.contentResolver, it)
+        if (bitmap != null) showItemDialog = true
+    }
+
+    val deleteStatus by viewModel.deleteStatus
+    var showDeleteDialog by remember { mutableStateOf(false) }
+    var selectedItem by remember { mutableStateOf<GalleryItem?>(null) }
+    LaunchedEffect(deleteStatus) {
+        if (deleteStatus != null) {
+            Toast.makeText(context, deleteStatus, Toast.LENGTH_SHORT).show()
+            viewModel.clearDeleteStatus()
+        }
+    }
+    var showEditDialog by remember { mutableStateOf(false) }
+
+    LaunchedEffect(Unit) {
+        viewModel.retrieveData()
+    }
+
+//    LaunchedEffect(user.email) {
+//        if (user.email.isNotEmpty()) {
+//            viewModel.retrieveData()
+//        }
+//    }
+
+    Scaffold (
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = stringResource(R.string.app_name))
+                },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                actions = {
+                    IconButton(onClick = {
+                        if (user.email.isEmpty()) {
+                            CoroutineScope(Dispatchers.IO).launch {
+                                signIn(context, dataStore)
+                            }
+                        } else {
+//                            Log.d("SIGN-IN", "User: $user")
+                            showDialog = true
+                        }
+                    })
+                    {
+                        Icon(
+                            painter = painterResource(id = R.drawable.account_circle),
+                            contentDescription = stringResource(id = R.string.profil),
+                            tint = MaterialTheme.colorScheme.primary
+                        )
+                    }
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = {
+                val options = CropImageContractOptions(
+                    null,
+                    CropImageOptions(
+                        imageSourceIncludeGallery = false,
+                        imageSourceIncludeCamera = true,
+                        fixAspectRatio = true
+                    )
+                )
+
+                launcher.launch(options)
+            }) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = stringResource(id = R.string.tambah_hewan)
+                )
+            }
+        }
+    ){
+            innerPadding ->
+        ScreenContent(
+            viewModel = viewModel,
+            modifier = Modifier.padding(innerPadding),
+            userId = user.email,
+            onDeleteClick = {
+                selectedItem = it
+                showDeleteDialog = true
+            },
+            onEditClick = {
+                selectedItem = it
+                showEditDialog = true
+            }
+        )
+
+        if (showDialog) {
+            ProfilDialog(
+                user = user,
+                onDismissRequest = { showDialog = false }
+            ) {
+                CoroutineScope(Dispatchers.IO).launch {
+                    signOut(context, dataStore)
+                }
+                showDialog = false
+            }
+        }
+        if (showItemDialog) {
+            GalleryDialog(
+                bitmap = bitmap,
+                onDismissRequest = { showItemDialog = false }
+            ) { title, description ->
+                viewModel.saveData(title, description, bitmap!!)
+                showItemDialog = false
+            }
+        }
+        if (showDeleteDialog && selectedItem != null) {
+            DeleteConfirmDialog(
+                item = selectedItem!!,
+                onDismiss = {
+                    showDeleteDialog = false
+                    selectedItem = null
+                },
+                onConfirm = {
+                    viewModel.deleteData(selectedItem!!.id)
+                    showDeleteDialog = false
+                    selectedItem = null
+                }
+            )
+        }
+
+        if (showEditDialog && selectedItem != null) {
+            val bitmap = remember(selectedItem) {
+                BitmapFactory.decodeFile(selectedItem!!.imagePath)
+            }
+
+            GalleryEditDialog(
+                bitmap = bitmap,
+                initialTitle = selectedItem!!.title,
+                initialDescription = selectedItem!!.description,
+                onDismissRequest = {
+                    showEditDialog = false
+                    selectedItem = null
+                },
+                onUpdate = { newTitle, newDescription ->
+                    viewModel.updateData(
+                        selectedItem!!.id,
+                        newTitle,
+                        newDescription,
+                        bitmap
+                    )
+                    showEditDialog = false
+                    selectedItem = null
+                }
+            )
+        }
+
+
+        if (errorMessage != null) {
+            Toast.makeText(context, errorMessage, Toast.LENGTH_LONG).show()
+            viewModel.clearMessage()
+        }
+    }
+}
+
+@Composable
+fun ScreenContent(
+    viewModel: MainViewModel,
+    modifier: Modifier = Modifier,
+    userId: String,
+    onDeleteClick: (GalleryItem) -> Unit,
+    onEditClick: (GalleryItem) -> Unit
+) {
+    val data by viewModel.data
+    val status by viewModel.status.collectAsState()
+
+
+    when {
+        status == ApiStatus.LOADING -> {
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator()
+            }
+        }
+
+        status == ApiStatus.ERROR && data.isEmpty() -> {
+            Column(
+                modifier = modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = stringResource(id = R.string.error))
+                Button(
+                    onClick = { viewModel.retrieveData() },
+                    modifier = Modifier.padding(top = 16.dp),
+                    contentPadding = PaddingValues(horizontal = 32.dp, vertical = 16.dp)
+                ) {
+                    Text(text = stringResource(id = R.string.try_again))
+                }
+            }
+        }
+
+        else -> {
+            LazyVerticalGrid(
+                modifier = modifier.fillMaxSize().padding(4.dp),
+                columns = GridCells.Fixed(2),
+                contentPadding = PaddingValues(bottom = 80.dp)
+            ) {
+                items(data) {
+                    ListItem(
+                        item = it,
+                        isUserLoggedIn = userId.isNotEmpty(),
+                        onDeleteClick = { onDeleteClick(it) },
+                        onEditClick = { onEditClick(it) }
+                    )
+                }
+            }
+        }
+    }
+
+
+}
+
+
+private suspend fun signIn(context: Context, dataStore: UserDataStore) {
+    val googleIdOption: GetGoogleIdOption = GetGoogleIdOption.Builder()
+        .setFilterByAuthorizedAccounts(false)
+        .setServerClientId(BuildConfig.API_KEY)
+        .build()
+
+    val request: GetCredentialRequest = GetCredentialRequest.Builder()
+        .addCredentialOption(googleIdOption)
+        .build()
+    try {
+        val credentialManager = CredentialManager.create(context)
+        val result = credentialManager.getCredential(context, request)
+        handleSignIn(result, dataStore)
+    } catch (e: GetCredentialException) {
+        Log.e("SIGN-IN", "Error: ${e.errorMessage}")
+    }
+}
+
+private suspend fun handleSignIn(result: GetCredentialResponse, dataStore: UserDataStore) {
+    val credential = result.credential
+    if (credential is CustomCredential &&
+        credential.type == GoogleIdTokenCredential.TYPE_GOOGLE_ID_TOKEN_CREDENTIAL) {
+        try {
+            val googleId = GoogleIdTokenCredential.createFrom(credential.data)
+            val nama = googleId.displayName ?: ""
+            val email = googleId.id
+            val photoUrl = googleId.profilePictureUri.toString()
+            dataStore.saveData(
+                User(
+                    name = nama,
+                    email = email,
+                    photoUrl = photoUrl
+                )
+            )
+        } catch (e: GoogleIdTokenParsingException) {
+            Log.e("SIGN-IN", "Error: ${e.message}")
+        }
+    } else {
+        Log.e("SIGN-IN", "Error: unrecognized custom credential type.")
+    }
+}
+
+private suspend fun signOut(context: Context, dataStore: UserDataStore) {
+    try {
+        val credentialManager = CredentialManager.create(context)
+        credentialManager.clearCredentialState(
+            ClearCredentialStateRequest()
+        )
+        dataStore.saveData(User("", "", ""))
+    } catch (e: ClearCredentialException) {
+        Log.e("SIGN-IN", "Error: ${e.errorMessage}")
+    }
+}
+
+private fun getCroppedImage(
+    resolver: ContentResolver,
+    result: CropImageView.CropResult
+): Bitmap? {
+    if (!result.isSuccessful) {
+        Log.e("IMAGE", "Error: ${result.error}")
+        return null
+    }
+
+    val uri = result.uriContent ?: return null
+
+    return if (Build.VERSION.SDK_INT < Build.VERSION_CODES.P) {
+        MediaStore.Images.Media.getBitmap(resolver, uri)
+    } else {
+        val source = ImageDecoder.createSource(resolver, uri)
+        ImageDecoder.decodeBitmap(source)
+    }
+}
+
+@Composable
+fun ListItem(
+    item: GalleryItem,
+    isUserLoggedIn: Boolean,
+    onDeleteClick: (() -> Unit)? = null,
+    onEditClick: (() -> Unit)? = null
+) {
+    Box(
+        modifier = Modifier
+            .padding(4.dp)
+            .border(1.dp, Color.Gray),
+        contentAlignment = Alignment.BottomCenter
+    ) {
+        AsyncImage(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(GalleryApi.getFullImageUrl(item.imagePath) + "?t=${System.currentTimeMillis()}")
+                .crossfade(enable = true)
+                .build(),
+            contentDescription = stringResource(R.string.gambar, item.title),
+            contentScale = ContentScale.Crop,
+            placeholder = painterResource(id = R.drawable.loading_img),
+            error = painterResource(id = R.drawable.broken_img),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(4.dp)
+        )
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(4.dp)
+                .background(Color(0f, 0f, 0f, 0.5f))
+                .padding(4.dp)
+        ) {
+            Column(
+                modifier = Modifier.align(Alignment.CenterStart)
+            ) {
+                Text(
+                    text = item.title,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.White
+                )
+                Text(
+                    text = item.description,
+                    fontStyle = FontStyle.Italic,
+                    fontSize = 14.sp,
+                    color = Color.White
+                )
+            }
+
+            // 🔐 Hanya tampilkan jika user login
+            if (isUserLoggedIn) {
+                Row(modifier = Modifier.align(Alignment.CenterEnd)) {
+                    if (onEditClick != null) {
+                        IconButton(
+                            onClick = onEditClick,
+                            modifier = Modifier.size(32.dp)
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.Edit,
+                                contentDescription = stringResource(R.string.edit),
+                                tint = Color.White,
+                                modifier = Modifier.size(20.dp)
+                            )
+                        }
+                    }
+
+                    if (onDeleteClick != null) {
+                        IconButton(
+                            onClick = onDeleteClick,
+                            modifier = Modifier.size(32.dp)
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.Delete,
+                                contentDescription = stringResource(R.string.hapus),
+                                tint = Color.White,
+                                modifier = Modifier.size(20.dp)
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
+
+@Preview(showBackground = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
+@Composable
+fun MainScreenPreview() {
+    Mobpro1Theme {
+        MainScreen()
+    }
+}
