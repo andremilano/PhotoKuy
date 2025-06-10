deac4a594e42d39edcb4cf56c132246c9f79578f
IGNP Andre Pratama Milano <andremilano16@gmail.com>
2025-06-10 11:55:39 +0700

    menambahkan model User dan GalleryItem serta dialog untuk mengedit dan mengonfirmasi penghapusan galeri

---
+++ app/src/main/java/com/andre0016/mobpro1/model/GalleryItem.kt
@@ -0,0 +1,9 @@
+package com.andre0016.mobpro1.model
+
+data class GalleryItem(
+    val id: String,
+    val title: String,
+    val description: String,
+    val imagePath: String,
+    val createdAt: String
+)
