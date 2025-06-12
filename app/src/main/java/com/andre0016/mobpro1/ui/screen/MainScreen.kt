9fa83b0758080f4c2dd0214eb91d6baf254d1459
IGNP Andre Pratama Milano <andremilano16@gmail.com>
2025-06-13 02:23:13 +0700

    membersihkan import tidak terpakai

--- app/src/main/java/com/andre0016/mobpro1/ui/screen/MainScreen.kt
+++ app/src/main/java/com/andre0016/mobpro1/ui/screen/MainScreen.kt
@@ -12,7 +12,6 @@ import android.util.Log
 import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
